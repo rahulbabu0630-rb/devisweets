0871171853e5647454764143ce978f8f4d597289
rahulbabu0630-rb <rb84502@gmail.com>
2025-05-22 00:01:04 +0530

    Update BulkAttendanceRepository.java

--- src/main/java/com/sweetshop/attendance/bulk/BulkAttendanceRepository.java
+++ src/main/java/com/sweetshop/attendance/bulk/BulkAttendanceRepository.java
@@ -1,7 +1,5 @@
- package com.sweetshop.attendance.bulk;
-
+package com.sweetshop.attendance.bulk;
 import com.sweetshop.attendance.model.Attendance; import org.springframework.data.jpa.repository.JpaRepository; import org.springframework.stereotype.Repository;
 
 @Repository public interface BulkAttendanceRepository extends JpaRepository<Attendance, Long> {
-
 }
