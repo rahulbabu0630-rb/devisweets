0fb3ed7fdc54bfc72614e6ca99f64fd710f003b0
rahulbabu0630-rb <rb84502@gmail.com>
2025-05-22 00:12:39 +0530

    Update AttendanceRepository.java

--- src/main/java/com/sweetshop/attendance/repository/AttendanceRepository.java
+++ src/main/java/com/sweetshop/attendance/repository/AttendanceRepository.java
@@ -1,13 +1,10 @@
 package com.sweetshop.attendance.repository;
-
 import com.sweetshop.attendance.model.Attendance;
 import com.sweetshop.attendance.model.Employee;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
