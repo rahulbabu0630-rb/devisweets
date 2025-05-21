0358e36f13e564496aebe02eb3201d94e82c34e3
rahulbabu0630-rb <rb84502@gmail.com>
2025-05-22 00:05:36 +0530

    Update AttendanceController.java

--- src/main/java/com/sweetshop/attendance/controller/AttendanceController.java
+++ src/main/java/com/sweetshop/attendance/controller/AttendanceController.java
@@ -1,14 +1,10 @@
 package com.sweetshop.attendance.controller;
-
 import com.sweetshop.attendance.model.Attendance;
-
 import com.sweetshop.attendance.repository.AttendanceRepository;
 import com.sweetshop.attendance.service.AttendanceService;
-
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
